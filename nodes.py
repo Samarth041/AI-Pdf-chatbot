9265af1652e14c793c319212f8f304970282d665
Samarth041 <samarthgupta826@gmail.com>
2026-07-14 13:34:27 +0530

    created nodes of graph retrieve and generate

---
+++ nodes.py
@@ -0,0 +1,36 @@
+from retriever import retrieve_documents
+
+from prompts import RAG_PROMPT
+from utils import chat_model
+
+def retrieve(state):
+    question=state["question"]
+
+    docs=retrieve_documents(question)
+
+    return{
+        "question":question,
+        "documents":docs
+    }
+
+def generate(state):
+
+    question=state["question"]
+
+    docs=state["documents"]
+
+    context="\n\n".join(
+        docs.page_content
+        for doc in docs
+    )
+
+    prompt=RAG_PROMPT.format(
+        context=context,
+        question=question
+    )
+
+    respone=chat_model.invoke(prompt)
+
+    return {
+        "answer":response.content
+    }
